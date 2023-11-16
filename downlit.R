7752952adeebca5a0f6ff95997ceae47e89755e9
simonpcouch <simonpatrickcouch@gmail.com>
2023-11-16 15:58:33 -0600

    apply downlit to example file

---
+++ downlit.R
@@ -0,0 +1,4 @@
+# apply `downlit_html()` to the example file so that folks can check out
+# documentation for the functions used there.
+
+downlit::downlit_html_path("docs/example/index.html", "docs/example/index.html")
